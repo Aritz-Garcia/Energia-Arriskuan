15a4978feb0e99a15503d98ac47896b2dd262fc5
Aritz-Garcia <aritz2030@gmail.com>
2023-12-19 12:28:34 +0100

    partida db

---
+++ energiaArriskuan/database/migrations/2023_12_19_111224_create_partidas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('partidas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_erabiltzailea');
+            $table->foreign('id_erabiltzailea')->references('id')->on('users');
+            $table->string('denbora')->nullable();
+            $table->boolean('bukatuta')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('partidas');
+    }
+};
