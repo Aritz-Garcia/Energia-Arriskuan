b4e3b96e2611d342f54d3748d13ef2964a4cd27e
Aritz-Garcia <aritz2030@gmail.com>
2023-12-20 09:21:11 +0100

    menu

--- energiaArriskuan/routes/web.php
+++ energiaArriskuan/routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('index');
 
 // Route::get('/perfil', function () {
 //     return view('perfil');
