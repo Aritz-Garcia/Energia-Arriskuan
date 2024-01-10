ddaadb8c56819a431a3e508d12ab5573c6df1951
Irkus <irkusanzola99@gmail.com>
2024-01-10 16:43:56 +0100

    logito arriba

--- energiaArriskuan/resources/views/layouts/plantillaHasiera.blade.php
+++ energiaArriskuan/resources/views/layouts/plantillaHasiera.blade.php
@@ -7,8 +7,10 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap">
     <title>@yield('title')</title>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="icon" href="{{ asset('images/Logo.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('images/Logo.png') }}" type="image/x-icon">
     <script src="{{ asset('js/app.js') }}" defer></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://cdn.tailwindcss.com"></script>
