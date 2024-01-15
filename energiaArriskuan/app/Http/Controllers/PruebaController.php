96e37671804bdcb78f42d0d952d3c805c4e6e095
Irkus Anzola <irkusanzola99@gmail.com>
2024-01-15 13:08:40 +0100

    pull

---
+++ energiaArriskuan/app/Http/Controllers/PruebaController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Prueba;
+
+
+class PruebaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(int $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    public function prueba3(String $partidaId){
+        
+        $prueba = Prueba::where("id_partida", $partidaId)->where("izena", "prueba3")->update(["bukatuta", 1]);
+
+        return redirect()->route("hasiera", $partidaId);
+
+    }
+}
