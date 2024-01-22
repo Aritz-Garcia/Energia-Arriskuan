e67f18fb6372ca6372643e2e98f55a842e448902
Irkus Anzola <irkusanzola99@gmail.com>
2024-01-22 10:31:05 +0100

    juego 2 principio

--- energiaArriskuan/resources/components/papelesPatio.vue
+++ energiaArriskuan/resources/components/papelesPatio.vue
@@ -1,15 +1,46 @@
 <template>
-    <div class="absolute text-white top-0">
-        <p>{{ simbolos }}</p>
-    </div>
+  <div class="absolute top-1/2 left-1/2">
+    <div class="flex items-center justify-center absolute gap-10 text-black rounded ateaItxiaText text-[50px]">
 
+        <p id="text" class="pl-14 pr-10 pt-5 pb-4">{{ textoMostrado }}</p>
+
+        <img
+        @click="closeGame"
+        id="close-img"
+        class="close-img"
+        src="../../public/images/cerrar.png"/>
+
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "papelesPatio",
-    props:{
-        simbolos : String,
+  name: "papelesPatio",
+
+  props: {
+    simbolos: String,
+  },
+
+  computed: {
+    textoMostrado() {
+      if (this.simbolos === "simbolo1") {
+        return "Φ";
+      } else if (this.simbolos === "simbolo2") {
+        return "λ";
+      } else if (this.simbolos === "simbolo3") {
+        return "〆";
+      } else if (this.simbolos === "simbolo4") {
+        return "Թ";
+      }
     },
-  }
+  },
+
+  methods: {
+    closeGame() {
+      document.getElementById("simbolosDiv").classList.remove("block");
+      document.getElementById("simbolosDiv").classList.add("hidden");
+    },
+  },
+};
 </script>
