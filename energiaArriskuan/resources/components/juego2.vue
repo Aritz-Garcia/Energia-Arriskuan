59d3adaa8180ca6285f0c6c76269d0dc5a60a89f
Irkus <irkusanzola99@gmail.com>
2024-01-24 20:12:00 +0100

    juego 2 sin terminar

--- energiaArriskuan/resources/components/juego2.vue
+++ energiaArriskuan/resources/components/juego2.vue
@@ -2,7 +2,11 @@
     <div>
         <div class="flex items-center justify-center mb-8">
             <p class="text-3xl md:w-auto mx-10 pl-20 p-6" id="text">
-                Aerosorgailua biratu daiteke. Azmatu kokapena kodea lortzeko.
+                Bilatu zein den panelaren inklinazioa eta orientazioa.
+                <!--
+                    inklinazioa (β)
+                    orientazioa (α)
+                -->
             </p>
             <img
                 id="close-img"
@@ -14,20 +18,31 @@
         </div>
 
         <div class="border-[#0BD904] border-2 rounded-xl p-4 text-white bg-[#010440] w-[880px] mx-auto">
-            <div class="flex">
+            <div class="flex justify-center mt-10">
 
                 <div class="flex flex-col items-center mx-5">
                     <div class="relative border-[#0BD904] border-2 rounded-xl p-5">
-                        <div class="h-[365px] w-[485px]">
-                            <img src="../../public/images/juego2/panelSolar.png" alt="hierba" class="absolute h-[405px] top-0 left-0 rounded-xl">
+                        <div class="h-[210px] w-[485px]">
+                            <img src="../../public/images/juego2/panelSolar.png" alt="hierba" class="absolute h-[250px] w-full top-0 left-0 rounded-xl">
                         </div>
                     </div>
 
-                    <!-- TODO Poner hover a los botones y un background -->
-                    <div class="flex items-center mt-5">
-                        <button @click.prevent="decrementarValor" class="border rounded-md border-[#0BD904] px-3 py-1 m-2">&#60;</button>
-                        <input type="range" name="brujula" id="brujulaSlider" min="0" max="359" value="0" @input="slideBrujula()" class="accent-[#0BD904]">
-                        <button @click.prevent="incrementarValor" class="border rounded-md border-[#0BD904] px-3 py-1 m-2">&#62;</button>
+                    <div class="flex gap-20">
+
+                        <div class="flex items-center mt-20 mb-14">
+                            <p class="text-xl">Inklinazioa:</p>
+                            <button @click.prevent="decrementarValor" class="border rounded-md border-[#0BD904] px-3 py-1 m-2">&#60;</button>
+                            <input type="range" name="inclinacion" id="brujulaSlider" min="0" max="100" value="50" @input="" class="accent-[#0BD904] m-[-50px] transform rotate-90 h-16">
+                            <button @click.prevent="incrementarValor" class="border rounded-md border-[#0BD904] px-3 py-1 m-2">&#62;</button>
+                        </div>
+
+                        <div class="flex items-center mt-20 mb-14">
+                            <p class="text-xl">Orientazioa:</p>
+                            <button @click.prevent="decrementarValor" class="border rounded-md border-[#0BD904] px-3 py-1 m-2">&#60;</button>
+                            <input type="range" name="orientacion" id="brujulaSlider" min="0" max="359" value="0" @input="" class="accent-[#0BD904]">
+                            <button @click.prevent="incrementarValor" class="border rounded-md border-[#0BD904] px-3 py-1 m-2">&#62;</button>
+                        </div>
+                        
                     </div>
                 </div>
 
