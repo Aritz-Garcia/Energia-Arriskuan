c5a92c984d88d27a05f4f664226f83ed6a62e5b2
Irkus <irkusanzola99@gmail.com>
2024-01-24 17:06:32 +0100

    juego2

--- energiaArriskuan/resources/components/juego2.vue
+++ energiaArriskuan/resources/components/juego2.vue
@@ -15,13 +15,11 @@
 
         <div class="border-[#0BD904] border-2 rounded-xl p-4 text-white bg-[#010440] w-[880px] mx-auto">
             <div class="flex">
+
                 <div class="flex flex-col items-center mx-5">
                     <div class="relative border-[#0BD904] border-2 rounded-xl p-5">
                         <div class="h-[365px] w-[485px]">
-                            <img src="../../public/images/juego4/hierba.jpg" alt="hierba" class="absolute h-[405px] top-0 left-0 rounded-xl">
-                            <img id="molinoImg" src="../../public/images/juego4/molino1.png" alt="molino" class="absolute left-[147px] w-48">
-                            <img src="../../public/images/juego4/viento.png" alt="viento" class="absolute top-[200px] left-[420px] w-16 -rotate-45 opacity-0" :style="{'animation': animacion}">
-                            <img src="../../public/images/juego4/pantalla.gif" alt="estatica de pantalla" class="absolute h-[405px] top-0 left-0 rounded-xl opacity-55">
+                            <img src="../../public/images/juego2/panelSolar.png" alt="hierba" class="absolute h-[405px] top-0 left-0 rounded-xl">
                         </div>
                     </div>
 
@@ -30,22 +28,6 @@
                         <button @click.prevent="decrementarValor" class="border rounded-md border-[#0BD904] px-3 py-1 m-2">&#60;</button>
                         <input type="range" name="brujula" id="brujulaSlider" min="0" max="359" value="0" @input="slideBrujula()" class="accent-[#0BD904]">
                         <button @click.prevent="incrementarValor" class="border rounded-md border-[#0BD904] px-3 py-1 m-2">&#62;</button>
-                    </div>
-                </div>
-
-                <div class="flex flex-col items-center mx-5">
-                    <div class="relative flex justify-between">
-                        <div class="border-2 border-[#0BD904] rounded-xl p-5">
-                            <div class="relative w-48 h-48">
-                                <img id="brujulaImg" src="../../public/images/juego4/brujula_general.png" alt="brujula" class="object-contain">
-                                <img src="../../public/images/juego4/brujula_aguja.png" alt="brujula aguja" class="absolute w-8 top-[33.5px] left-[80px]">
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- TODO Cambiar estilo de los botes de arriba y abajo y el boton de corregir -->
-                    <div id="panelNumerico">
-                        <Panel :partida="partida"></Panel>
                     </div>
                 </div>
 
