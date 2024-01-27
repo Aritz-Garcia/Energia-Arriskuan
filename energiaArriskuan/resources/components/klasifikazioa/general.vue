63bf13caf321f198dfe267bdaae9c3306a17b44a
Aritz-Garcia <73273260+Aritz-Garcia@users.noreply.github.com>
2024-01-27 18:38:31 +0100

    klasifikazioa

--- energiaArriskuan/resources/components/klasifikazioa/general.vue
+++ energiaArriskuan/resources/components/klasifikazioa/general.vue
@@ -1,9 +1,44 @@
 <template>
-    <div>
-        <h1>Generala</h1>
+    <div class="mt-5">
+        <h1 class="text-center text-2xl text-[#0BD904]">Orokorra</h1>
+        <table class="w-full text-center border my-3 bg-[#010440] text-[#0BD904]">
+            <thead>
+                <tr class="border">
+                    <th class="border">Postua</th>
+                    <th class="border">Erabiltzailea</th>
+                    <th class="border">Denbora</th>
+                </tr>
+            </thead>
+            <tbody class="font-rubik-regular">
+                <tr v-for="(partida, index) in partidakConErabiltzaileak()" :key="index" class="border">
+                    <td class="border">{{ index + 1 }}</td>
+                    <td class="border">{{ partida.erabiltzailea }}</td>
+                    <td class="border">{{ partida.denbora }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script>
-    
+    export default {
+        name: "general",
+        props: {
+            partidak: Array,
+            erabiltzaileak: Array,
+            probak: Array,
+        },
+        methods: {
+            partidakConErabiltzaileak() {
+                return this.partidak.map(partida => {
+                    const erabiltzailea = this.erabiltzaileak.find(e => e.id == partida.id_erabiltzailea);
+                    return {
+                        ...partida,
+                        erabiltzailea: erabiltzailea ? erabiltzailea.erabiltzailea : '',
+                        argazkia: erabiltzailea ? erabiltzailea.foto : '',
+                    };
+                });
+            }
+        }
+    }
 </script>
--- energiaArriskuan/resources/components/klasifikazioa/proba4.vue
+++ energiaArriskuan/resources/components/klasifikazioa/proba4.vue
@@ -1,9 +1,50 @@
 <template>
-    <div>
-        <h1>Proba 4</h1>
+    <div class="mt-5">
+        <h1  class="text-center text-2xl text-[#0BD904]">4. Proba</h1>
+        <table class="w-full text-center border my-3 bg-[#010440] text-[#0BD904]">
+            <thead>
+                <tr class="border">
+                    <th class="border">Postua</th>
+                    <th class="border">Erabiltzailea</th>
+                    <th class="border">Denbora</th>
+                </tr>
+            </thead>
+            <tbody class="font-rubik-regular">
+                <tr v-for="(proba, index) in proba4Erabiltzaileak()" :key="index" class="border">
+                    <td class="border">{{ index + 1 }}</td>
+                    <td class="border">{{ proba.erabiltzailea }}</td>
+                    <td class="border">{{ proba.denbora }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script>
-    
+    export default {
+        name: "general",
+        props: {
+            partidak: Array,
+            erabiltzaileak: Array,
+            probak: Array,
+        },
+        computed: {
+            proba1() {
+                return this.probak.filter(proba => proba.izena == "prueba4");
+            }
+        },
+        methods: {
+            proba4Erabiltzaileak() {
+                return this.proba1.map(proba => {
+                    const partida = this.partidak.find(p => p.id == proba.id_partida);
+                    const erabiltzailea = this.erabiltzaileak.find(e => e.id == partida.id_erabiltzailea);
+                    return {
+                        ...proba,
+                        erabiltzailea: erabiltzailea ? erabiltzailea.erabiltzailea : '',
+                        argazkia: erabiltzailea ? erabiltzailea.foto : '',
+                    };
+                });
+            }
+        }
+    }
 </script>
