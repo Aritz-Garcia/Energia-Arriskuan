19fee1878d1c8fdb1f90ccc2a7d89da9bd7c7831
Aritz-Garcia <73273260+Aritz-Garcia@users.noreply.github.com>
2024-01-28 14:43:33 +0100

    game over update

--- energiaArriskuan/resources/views/partida/gameOver.blade.php
+++ energiaArriskuan/resources/views/partida/gameOver.blade.php
@@ -6,7 +6,7 @@
 
     <div class="w-screen h-screen sotoaPanelLetra gameOver absolute top-1/2 left-1/2">
         <p class="text-red-600 text-5xl">Game Over</p>
-        <p class="font-digital-regular text-red-600 text-3xl">Ezin izan duzu energia-sistemak berrezartu. <br>Denbora bukatu zaizu eta ezin izan zara bizirik mantendu.</p>
+        <p class="font-digital-regular text-red-600 text-3xl mt-4">Ezin izan duzu energia-sistemak berrezartu. <br>Denbora bukatu zaizu eta ezin izan zara bizirik mantendu.</p>
 
         <a href="{{ route('index') }}" id="bottone1" class="md:mx-auto flex items-center text-[#010440]">
             <strong class="text-[#010440] font-rubik-bold">ITZULI</strong>
