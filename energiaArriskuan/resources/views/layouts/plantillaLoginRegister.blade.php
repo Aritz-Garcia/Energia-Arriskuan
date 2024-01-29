ae202a28f20adfe864e541b11a4309b32a31eb1a
Aritz-Garcia <73273260+Aritz-Garcia@users.noreply.github.com>
2024-01-29 20:42:45 +0100

    fomualrio info

--- energiaArriskuan/resources/views/layouts/plantillaLoginRegister.blade.php
+++ energiaArriskuan/resources/views/layouts/plantillaLoginRegister.blade.php
@@ -7,7 +7,7 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="{{ asset('js/app.js') }}" defer></script>
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/sass.css'])
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/sass.css', 'resources/js/loginRegistro.js'])
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <link rel="icon" href="{{ asset('images/Logo.png') }}" type="image/x-icon">
@@ -35,8 +35,8 @@
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
             </div>
-            <div class="w-full max-w-sm sm:max-w-lg mt-4 px-6 py-2 my-20 formFondo shadow-md overflow-hidden sm:rounded-lg">
-                @yield('content')
+            
+            @yield('content')
             </div>
         </div>
     </main>
