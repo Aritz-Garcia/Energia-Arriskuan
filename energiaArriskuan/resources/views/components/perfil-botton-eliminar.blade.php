b6b203a8b5a66defd65098b49db29e0589115687
Irkus Anzola <irkusanzola99@gmail.com>
2024-01-29 09:48:55 +0100

    cosas

--- energiaArriskuan/resources/views/components/perfil-botton-eliminar.blade.php
+++ energiaArriskuan/resources/views/components/perfil-botton-eliminar.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'mt-4 text-center border-solid border-2 border-[#ff3131] rounded-full p-2 text-[#010440] bg-[#ff3131] font-semibold w-auto sm:w-36']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'mt-4 text-center border-solid border-2 border-[#ff3131] rounded-full p-2 text-[#010440] bg-[#ff3131] hover:bg-[#fff] hover:border-[#fff] font-semibold w-auto sm:w-36']) }}>
     {{ $slot }}
 </button>
