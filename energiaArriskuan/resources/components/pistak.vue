7fda4f5e7ab106096adb0b0cde3c91dc23e04661
Adri22 <adrianocampose22@gmail.com>
2024-01-31 09:13:03 +0100

    Pistas Completado

--- energiaArriskuan/resources/components/pistak.vue
+++ energiaArriskuan/resources/components/pistak.vue
@@ -9,105 +9,62 @@
             </div>
         </div>
 
-      <div v-if="confirmacion" class="confirmation-box">
-          <h2 class="texto">Ziur zaude pista erabili nahi duzula?</h2>
-          <div class="botonesc">
-              <button class="font-semibold text-[#010440] bg-[#0BD904] hover:bg-white hover:text-[#010440] py-2 px-4 rounded-full focus:outline-none" @click.prevent="enseñarPista">Bai</button>
-              <button class="font-semibold text-[#010440] bg-[#d90404] hover:bg-white hover:text-[#010440] py-2 px-4 rounded-full focus:outline-none" @click.prevent="confirmacion = false">Ez</button>
+      <div v-if="confirmacion" id="modal" class="fixed bg-gray-900 p-5 text-center">
+          <h2 class="text-white p-10 text-xl mb-5">Ziur zaude pista erabili nahi duzula?</h2>
+          <div id="botonesc" class="flex justify-center">
+              <button class="font-semibold testu-kolore-oinarria atzekaldeko-kolore-nagusia hover:bg-white hover:text-[#010440] py-2 px-4 rounded-full focus:outline-none" @click.prevent="enseñarPista">Bai</button>
+              <button class="font-semibold testu-kolore-oinarria bg-[#d90404] hover:bg-white hover:text-[#010440] py-2 px-4 rounded-full focus:outline-none" @click.prevent="confirmacion = false">Ez</button>
           </div>
       </div>
 
       <!-- Denbora itzatekotan testua -->
         <Teleport to="body">
-            <div v-if="abierto" class="modal">
-                <p class="texto">{{this.mensaje}}</p>
-                <button @click.prevent="quitartiempo" class="font-semibold text-[#010440] bg-[#d90404] hover:bg-white hover:text-[#010440] py-2 px-4 rounded-full focus:outline-none">Itxi</button>
+            <div v-if="abierto" id="modal" class="fixed bg-gray-900 p-5 text-center">
+                <p class="text-white p-10 text-xl">{{this.mensaje}}</p>
+                <button @click.prevent="quitartiempo" class="font-semibold testu-kolore-oinarria bg-[#d90404] hover:bg-white hover:text-[#010440] py-2 px-4 rounded-full focus:outline-none">Itxi</button>
             </div>
         </Teleport>
 
         <!-- Denbora gabe testua -->
         <Teleport to="body">
-            <div v-if="abierto2" class="modal">
-                <p class="texto">Ez daukazu denbora nahiko pista bat ikusteko</p>
-                <button @click.prevent="abierto2 = false" class="font-semibold text-[#010440] bg-[#d90404] hover:bg-white hover:text-[#010440] py-2 px-4 rounded-full focus:outline-none">Itxi</button>
+            <div v-if="abierto2" id="modal" class="fixed bg-gray-900 p-5 text-center">
+                <p class="text-white p-10 text-xl">Ez daukazu denbora nahiko pista bat ikusteko</p>
+                <button @click.prevent="abierto2 = false" class="font-semibold testu-kolore-oinarria bg-[#d90404] hover:bg-white hover:text-[#010440] py-2 px-4 rounded-full focus:outline-none">Itxi</button>
             </div>
         </Teleport>
+
+        <!-- Denbora gabe testua -->
+        <Teleport to="body">
+            <div v-if="abierto3" id="modal" class="fixed bg-gray-900 p-5 text-center">
+                <p class="text-white p-10 text-xl">Ez daukazu pista gehiagorik leku honetan</p>
+                <button @click.prevent="abierto3 = false" class="font-semibold testu-kolore-oinarria bg-[#d90404] hover:bg-white hover:text-[#010440] py-2 px-4 rounded-full focus:outline-none">Itxi</button>
+            </div>
+        </Teleport>
+
     </div>
+
+         
+    
 
 </template>
 <style>
-.modal {
-  position: fixed;
+
+#modal {
   z-index: 999;
   top: 50%;
   left: 50%;
   width: 300px;
-  background-color: #010440;
   border: 3px solid #0BD904;
-  padding: 20px;
-  text-align: center;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  /* z-index: 2; */
-  border-radius: 10px;
-
-  /* Añadir estas propiedades para centrar vertical y horizontalmente */
-  transform: translate(-50%, -50%);
-}
-
-.texto{
- color: white;
- padding: 40px;
- font-size: 20px;
-}
-
-.confirmation-box {
-  position: fixed;
-  z-index: 999;
-  top: 50%;
-  left: 50%;
-  width: 300px;
-  background-color: #010440;
-  border: 3px solid #0BD904;
-  padding: 20px;
   text-align: center;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   border-radius: 10px;
   transform: translate(-50%, -50%);
 }
 
-.confirmation-box-si {
-  display: block;
-  width: 300px;
-  background-color: #010440;
-  border: 3px solid #0BD904;
-  border-radius: 2px;
-  padding: 20px;
-  text-align: center;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  z-index: 3;
-  border-radius: 10px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-
-.botonesc{
-  display: flex;
-  justify-content: center;
+#botonesc{
   gap: 20px;
 }
 
-#confirmation-box h2 {
-  margin-bottom: 20px;
-
-}
-
-#confirmation-box button {
-  padding: 10px;
-  margin: 0 10px;
-  cursor: pointer;
-}
 
 </style>
 <script>
@@ -155,6 +112,7 @@
             mensaje: "",
             abierto: false,
             abierto2: false,
+            abierto3: false,
             confirmacion: false
             }
         },
@@ -170,17 +128,26 @@
                     this.confirmacion = false;
                 } else {
                     this.confirmacion = false;
+
                     switch (this.lugar) {
                         case "hasiera":
                             for (let i = 0; i < this.probak.length; i++) {
                                 if (this.probak[i].izena == "prueba3") {
                                     if (this.probak[i].bukatuta) {
+                                for (let i = 0; i < this.probak.length; i++) {
+                                        if (this.probak[i].izena == "prueba2") {
+                                    if (this.probak[i].bukatuta) {
+                                        this.abierto = false;
+                                        this.abierto3 = true;
+                                    } else {
                                         this.mensaje = this.pistak[0].hasiera[this.actual].pista;
                                         this.abierto = true;
-                                        this.abierto2 = false;
+                                        this.abierto3 = false;
+                                    }
+                                }}
                                     } else {
                                         this.abierto = true;
-                                        this.abierto2 = false;
+                                        this.abierto3 = false;
                                         this.mensaje = this.pistak[3].juego3[this.actual].pista; 
                                     }
                                     return;
@@ -193,10 +160,10 @@
                                 if (this.probak[i].izena == "prueba2") {
                                     if (this.probak[i].bukatuta) {
                                         this.abierto = false;
-                                        this.abierto2 = true;
+                                        this.abierto3 = true;
                                     } else {
                                         this.abierto = true;
-                                        this.abierto2 = false;
+                                        this.abierto3 = false;
                                         this.mensaje = this.pistak[5].patioa[this.actual].pista;
                                     }
                                     return;
@@ -209,10 +176,10 @@
                                 if (this.probak[i].izena == "prueba1") {
                                     if (this.probak[i].bukatuta) {
                                         this.abierto = false;
-                                        this.abierto2 = true;
+                                        this.abierto3 = true;
                                     } else {
                                         this.abierto = true;
-                                        this.abierto2 = false;
+                                        this.abierto3 = false;
                                         this.mensaje = this.pistak[1].juego1[this.actual].pista;
                                     }
                                     return;
@@ -225,10 +192,10 @@
                                 if (this.probak[i].izena == "prueba2") {
                                     if (this.probak[i].bukatuta) {
                                         this.abierto = false;
-                                        this.abierto2 = true;
+                                        this.abierto3 = true;
                                     } else {
                                         this.abierto = true;
-                                        this.abierto2 = false;
+                                        this.abierto3 = false;
                                         this.mensaje = this.pistak[2].juego2[this.actual].pista;
                                     }
                                     return;
@@ -241,10 +208,10 @@
                                 if (this.probak[i].izena == "prueba4") {
                                     if (this.probak[i].bukatuta) {
                                         this.abierto = false;
-                                        this.abierto2 = true;
+                                        this.abierto3 = true;
                                     } else {
                                         this.abierto = true;
-                                        this.abierto2 = false;
+                                        this.abierto3 = false;
                                         this.mensaje = this.pistak[4].juego4[this.actual].pista;
                                     }
                                     return;
